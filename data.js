0e5223af36302b0d126a06869c72c1648d5290d2
Conor Butcher <conorbutcher22@outlook.com>
2024-01-21 10:11:10 +0000

    Initial Tour Commit

---
+++ data.js
@@ -0,0 +1,331 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-camera-001",
+      "name": "Camera 001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.9002700205705398,
+        "pitch": 0.042555172465219115,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4155778216741517,
+          "pitch": 0.024645059991499352,
+          "rotation": 0,
+          "target": "1-camera-002"
+        },
+        {
+          "yaw": 1.9083472522729998,
+          "pitch": 0.012424664639864247,
+          "rotation": 1.5707963267948966,
+          "target": "6-camera-007---blurred"
+        },
+        {
+          "yaw": 3.098442807607527,
+          "pitch": -0.006835301382606573,
+          "rotation": 0,
+          "target": "6-camera-007---blurred"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-camera-002",
+      "name": "Camera 002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5502153122651983,
+          "pitch": -0.0022910351397289475,
+          "rotation": 0,
+          "target": "0-camera-001"
+        },
+        {
+          "yaw": 0.06651909214645357,
+          "pitch": 0.0044313032201603875,
+          "rotation": 0,
+          "target": "2-camera-003"
+        },
+        {
+          "yaw": 2.49604284163261,
+          "pitch": -0.046209198910640126,
+          "rotation": 0,
+          "target": "6-camera-007---blurred"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-camera-003",
+      "name": "Camera 003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0913464739447445,
+          "pitch": 0.007388772041807101,
+          "rotation": 0,
+          "target": "1-camera-002"
+        },
+        {
+          "yaw": -2.4110890952829482,
+          "pitch": 0.007530797715888937,
+          "rotation": 0,
+          "target": "0-camera-001"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-camera-005"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-camera-004",
+      "name": "Camera 004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5506169276587976,
+        "pitch": 0.07730685715706898,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5506169276587976,
+          "pitch": 0.07730685715706898,
+          "rotation": 0,
+          "target": "2-camera-003"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-camera-005",
+      "name": "Camera 005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0993646301524738,
+          "pitch": 0.027804445572080283,
+          "rotation": 0,
+          "target": "2-camera-003"
+        },
+        {
+          "yaw": -2.5825417240734048,
+          "pitch": 0.021907730012500792,
+          "rotation": 1.5707963267948966,
+          "target": "3-camera-004"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-camera-006"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-camera-006",
+      "name": "Camera 006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.427911557279625,
+        "pitch": 0.037190884638091504,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.801143501705724,
+          "pitch": 0.047524640591866785,
+          "rotation": 0,
+          "target": "4-camera-005"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-camera-007---blurred",
+      "name": "Camera 007 - Blurred",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5638780224683426,
+          "pitch": -0.008285091069122785,
+          "rotation": 0,
+          "target": "0-camera-001"
+        },
+        {
+          "yaw": 0.2665410359334537,
+          "pitch": -0.009494395747422502,
+          "rotation": 0,
+          "target": "1-camera-002"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
